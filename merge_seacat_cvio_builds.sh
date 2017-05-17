ba2fb076b04985f9497080bf844554a5668ee665
Miloslav Pavelka <miloslav.pavelka@teskalabs.com>
2017-05-17 15:36:17 +0200

    Adding cvio and seacat aar merge script.

---
+++ merge_seacat_cvio_builds.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+TMP=${BASEDIR}/tmp
+TMP_CVIO=${BASEDIR}/tmp/cvio
+TMP_SEACAT=${BASEDIR}/tmp/seacat
+TMP_BUILD=${BASEDIR}/tmp/build
+TMP_JAR=${BASEDIR}/tmp/tmp-jar
+
+
+CVIO_AAR_FILE=CatVision_Android_a78ca93-dirty-release.aar
+
+# Clean
+rm -rf ${BASEDIR}/tmp
+
+# Create dirs
+mkdir -p ${TMP_CVIO}
+mkdir -p ${TMP_SEACAT}
+mkdir -p ${TMP_BUILD}
+mkdir -p ${TMP_JAR}
+
+
+unzip ${BASEDIR}/seacat/SeaCatClient_Android_v1611-rc-3-release.aar -d ${TMP_SEACAT};
+unzip ${BASEDIR}/cvio/build/outputs/aar/${CVIO_AAR_FILE} -d ${TMP_CVIO};
+
+# Copy cvio
+cp -r ${TMP_CVIO}/* ${TMP_BUILD}/
+
+# Merge cvio and seacat jni
+cp -r ${TMP_SEACAT}/jni/* ${TMP_BUILD}/jni
+
+# Merge classes.jar
+(cd ${TMP_JAR}; jar -xf ${TMP_CVIO}/classes.jar)
+(cd ${TMP_JAR}; jar -xf ${TMP_SEACAT}/classes.jar)
+jar -cf ${TMP}/classes.jar -C ${TMP_JAR} .
+mv -f ${TMP}/classes.jar ${TMP_BUILD}/classes.jar
+
+# zip aar
+jar cvf ${CVIO_AAR_FILE} -C ${TMP_BUILD}/ .
