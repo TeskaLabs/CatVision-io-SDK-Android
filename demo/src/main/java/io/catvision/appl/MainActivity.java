33d7029427eefe34bc8bad491c0d9a967230165e
Přemysl Černý <premysl.cerny@teskalabs.com>
2018-03-07 10:11:27 +0100

    Using a standard way to create a menu.

--- demo/src/main/java/io/catvision/appl/MainActivity.java
+++ demo/src/main/java/io/catvision/appl/MainActivity.java
@@ -219,10 +219,35 @@ public class MainActivity extends AppCompatActivity implements StoppedFragment.O
 
 	// Menu ----------------------------------------------------------------------------------------
 
+	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.menu_main, menu);
 		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle item selection
+		switch (item.getItemId()) {
+			case R.id.menu_show_tag:
+				onMenuItemClickShowClientTag(item);
+				return true;
+			case R.id.menu_test_area:
+				onMenuItemClickTestArea(item);
+				return true;
+			case R.id.menu_reset:
+				onMenuItemClickResetIdentity(item);
+				return true;
+			case R.id.menu_help:
+				onMenuItemClickHelp(item);
+				return true;
+			case R.id.menu_about:
+				onMenuItemClickAbout(item);
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
+		}
 	}
 
 	public boolean onMenuItemClickShowClientTag(MenuItem v) {
